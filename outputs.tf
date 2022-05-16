f15f26a5ca7f2422ec81238b2fbc738381d0e899
paulinaMorenoA <105310671+paulinaMorenoA@users.noreply.github.com>
2022-05-16 16:27:41 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -20,5 +20,5 @@ output "svc_project" {
 
 output "projects_number" {
   description = "Project Numbers."
-  value = [for project in module.vpc_svc_project : project["vpc_svc_project"]["project_number"]]
+  value = module.vpc_svc_project["vpc_svc_project"]["project_number"]
 }
